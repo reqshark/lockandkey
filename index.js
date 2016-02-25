2fde5118ad057fc2d4774197f94e07863c3325fc
Bent Cardan <bent@nothingsatisfies.com>
2016-02-25 13:50:24 -0800

    :high_heel: style

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var crypto = require('crypto');
+const crypto = require('crypto')
 
 module.exports = {
-  lock: function (str,k){
-    var c = crypto.createCipher('aes-256-cbc',k);
-    var ret = c.update(str,'utf8','base64');
-    ret += c.final('base64');
-    return ret;
+  lock: ( str, k ) => {
+    var c = crypto.createCipher('aes-256-cbc',k)
+    var ret = c.update(str,'utf8','base64')
+    ret += c.final('base64')
+    return ret
   },
-  unlock: function (str,k){
-    var d = crypto.createDecipher('aes-256-cbc', k);
-    var ret = d.update(str,'base64','utf8');
-    ret += d.final('utf8');
-    return ret;
+  unlock: ( str, k ) => {
+    var d = crypto.createDecipher('aes-256-cbc', k)
+    var ret = d.update(str,'base64','utf8')
+    ret += d.final('utf8')
+    return ret
   }
 }
