0c0f44eb85553fded4f247462d21ec10b7e654d3
Bent Cardan <bent@nothingsatisfies.com>
2026-03-17 16:44:11 -0700

    fix string in error msg

--- test/index.js
+++ test/index.js
@@ -1,10 +1,8 @@
 // var key = require('./diffie');
 
-var key = '2cb1866097f2b6802b2ebef8ece2e088879a1494a9884b31893e7d87c81f0d08'
+let key = '2cb1866097f2b6802b2ebef8ece2e088879a1494a9884b31893e7d87c81f0d08'
 
-var lockandkey = require('../');
-var lock = lockandkey.lock;
-var unlock = lockandkey.unlock;
+const { lock, unlock } = require('../')
 
 var locked = lock('its a done deal, the cops didnt catch us.', Buffer.from(key, 'hex'));
 var unlocked = unlock(locked, Buffer.from(key, 'hex'));
