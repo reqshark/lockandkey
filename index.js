e414db20c598cb0dab51941e9d9d7744638310ad
Bent Cardan <bent@nothingsatisfies.com>
2026-03-17 16:40:29 -0700

    more modern stuff

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const {
-  createCipheriv,
-  createDecipheriv
-} = require('crypto')
+const { createCipheriv, createDecipheriv } = require('crypto')
 
-module.exports = {
-  lock: ( str, k ) => {
-    if (k.length !== 32)
-      throw new Error('key must be 32 bytes long\nlike:\nkey = randomBytes(32).toString('hex')')
-    const c = createCipheriv('aes-256-cbc', k, new Uint8Array(16))
-    return (c.update(str, 'utf8', 'hex') + c.final('hex'))
-  },
-  unlock: ( str, k ) => {
-    if (k.length !== 32)
-      throw new Error('key must be 32 bytes long\nlike:\nkey = randomBytes(32).toString('hex')')
-    const d = createDecipheriv('aes-256-cbc', k, new Uint8Array(16))
-    return (d.update(str,'hex', 'utf8') + d.final('utf8'))
-  }
+module.exports = { lock, unlock }
+
+function lock (str, k) {
+  if (k.length !== 32)
+    throw new Error('key must be 32 bytes long\nlike:\nkey = randomBytes(32).toString('hex')')
+  const c = createCipheriv('aes-256-cbc', k, new Uint8Array(16))
+  return (c.update(str, 'utf8', 'hex') + c.final('hex'))
+}
+
+function unlock (str, k) {
+  if (k.length !== 32)
+    throw new Error('key must be 32 bytes long\nlike:\nkey = randomBytes(32).toString('hex')')
+  const d = createDecipheriv('aes-256-cbc', k, new Uint8Array(16))
+  return (d.update(str,'hex', 'utf8') + d.final('utf8'))
 }
